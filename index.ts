c9cfb2e9521afba8517becb29469dd5588cd40ec
Juyeon-Ji <juyeonie216@gmail.com>
2021-02-22 17:46:35 +0900

    test success

--- index.ts
+++ index.ts
@@ -1,6 +1,23 @@
-// Import stylesheets
-import './style.css';
+function solution(array, commands) {
+  var answer = [];
+  commands.forEach(function(command) {
+    let [i, j, k] = command;
+    let i2jArr = [];
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+    i2jArr = array.slice(i - 1, j);
+    sort_ascending(i2jArr);
+    answer.push(i2jArr[k - 1]);
+  });
+  return answer;
+}
+
+function sort_ascending(arr) {
+  arr.sort(function(a, b) {
+    // 오름차순
+    return a - b;
+  });
+}
+
+let result = solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]);
+
+console.log("result", result);
